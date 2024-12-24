cebedba2d71c67d71639577e9a85eab7b8de701a
SuperHeraut <super.heraut.officiel@gmail.com>
2024-12-24 02:19:51 +0100

    chore(js/): edit mode.js

--- js/mode.js
+++ js/mode.js
@@ -21,5 +21,5 @@ function nightmode() {
 			break;
 	}
 	modepic.setAttribute("src", "../img/technical/" + modeinner + ".svg");
-	STYLESHEET.setAttribute("href", "../CSS/" + style + ".css");
+	MODE.setAttribute("href", "../CSS/" + style + ".css");
 };
