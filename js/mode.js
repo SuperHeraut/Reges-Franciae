9c4d7ffdd9ef7bece0f47e4667617a86d24cd882
SuperHeraut <super.heraut.officiel@gmail.com>
2025-01-16 04:21:18 +0100

    refactor(js/mode.js): change function to arrow function

--- js/mode.js
+++ js/mode.js
@@ -1,14 +1,37 @@
 modebutton = document.createElement("button");
 modepic = document.createElement("img");
-modebutton.appendChild(modepic);
 modepic.setAttribute("src", "../img/technical/" + modeinner + ".svg");
-HEADER.appendChild(modebutton);
 modebutton.setAttribute("onclick", "nightmode()");
-console.log(localStorage);
+HEADER.appendChild(modebutton);
+modebutton.appendChild(modepic);
+
+const nightmode = () => {
+	switch (mode) {
+		case "1":
+			style = "night";
+			localStorage.setItem("LSmode", mode);
+			mode = "0";
+			modeinner = "moon";
+			break;
+
+		case "0":
+			style = "clear";
+			localStorage.setItem("LSmode", mode);
+			mode = "1";
+			modeinner = "sun";
+			break;
+	}
+
+	console.log(localStorage.getItem("LSmode", mode));
+	modepic.setAttribute("src", "../img/technical/" + modeinner + ".svg");
+	MODE.setAttribute("href", "../css/" + style + ".css");
+};
 
 //localStorage used to keep ðe defined preferred þeme during ſeſsion.
 //ðe reﬅ sets ðe þeme.
-if(localStorage.getItem("LSmode", mode) == "0" || localStorage.getItem("LSmode", mode) == "1"){
+if(localStorage.getItem("LSmode", mode) == "0" ||
+	localStorage.getItem("LSmode", mode) == "1")
+{
 	mode = localStorage.getItem("LSmode", mode);
 	nightmode();
 } else {
@@ -17,24 +40,4 @@ if(localStorage.getItem("LSmode", mode) == "0" || localStorage.getItem("LSmode",
 	localStorage.setItem("LSmode", mode);
 	modeinner = "sun";
 	nightmode();
-};
-
-function nightmode() {
-	switch (mode) {
-		case "1":
-			style = "night";
-			localStorage.setItem("LSmode", mode);
-			mode = "0";
-			modeinner = "moon";
-			break;
-		case "0":
-			style = "clear";
-			localStorage.setItem("LSmode", mode);
-			mode = "1";
-			modeinner = "sun";
-			break;
-	}
-	console.log(localStorage.getItem("LSmode", mode));
-	modepic.setAttribute("src", "../img/technical/" + modeinner + ".svg");
-	MODE.setAttribute("href", "../css/" + style + ".css");
 };
